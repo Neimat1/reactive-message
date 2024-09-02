c4bb8939e8f0a882d6e196995fab1b263c55732a
Neimat1 <neimat.soliman.ismail@gmail.com>
2024-09-02 03:50:36 +0300

    rest-client: add endpoints

--- rest-client/src/main/java/com/quarkus/microservice/service/two/RestClientResource.java
+++ rest-client/src/main/java/com/quarkus/microservice/service/two/RestClientResource.java
@@ -1,16 +1,42 @@
 package com.quarkus.microservice.service.two;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.quarkus.microservice.service.two.proxy.MessageProxy;
+import com.quarkus.microservice.service.two.service.RestClientService;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Retry;
 
-@Path("/hello")
+@Path("/api/rest-client")
 public class RestClientResource {
 
+    @Inject
+    RestClientService service;
+
+
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Retry( delay = 3000, maxRetries = 4)
+    public Uni<Response> getWelcomeMessage() {
+        return service.getWelcomeMessage();
+    }
+
+    @POST
+    @Path("/create-message")
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Retry( delay = 3000, maxRetries = 4)
+    public Uni<Response> createMessage(String message) {
+        return service.createMessage(message);
+    }
+
+    @POST
+    @Path("/throw-exception")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Retry( delay = 3000, maxRetries = 4)
+    public Uni<Response> throwExceptionMessage() {
+        return service.throwExceptionMessage();
     }
 }
